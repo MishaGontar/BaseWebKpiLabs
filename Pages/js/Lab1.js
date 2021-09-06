3d557a7c820e70f21f0f453d7c0949f9dfaea842
Mischa <Resbybli1>
2021-09-06 12:49:57 +0300

    Initial commit

---
+++ Pages/js/Lab1.js
@@ -0,0 +1,36 @@
+function change(props) {
+    let a = document.getElementById('cont')
+    a.innerHTML = props
+}
+
+function about() {
+    const text = "<h1>Опис предметного середовища</h1>" +
+        ""
+    change(text)
+}
+
+function typeOfLab() {
+    const text = "<h1>Hello Misha this is page</h1>"
+    change(text)
+}
+
+function s_table() {
+    const text = "<div>Hello from s_table</div>"
+    change(text)
+}
+
+function s_form() {
+    const text = "<div>Hello from s_form</div>"
+    change(text)
+}
+
+function s_img() {
+    const text = "<div>Hello from s_img</div>"
+    change(text)
+}
+
+function conclusion() {
+    const text = "<div>ergqewgeqgqerugoeniogengpewogepognmqegoomeqrgmoqergqermgqergop[qerg[qer[mgmeqr[gqegpeqogqergqewgeqgqerugoeniogengpewogepognmqegoomeqrgmoqergqermgqergopergqewgeqgqerugoeniogengpewogepognmqegoomeqrgmoqergqermgqergopergqewgeqgqerugoeniogengpewogepognmqegoomeqrgmoqergqermgqergopergqewgeqgqerugoeniogengpewogepognmqegoomeqrgmoqergqermgqergopergqewgeqgqerugoeniogengpewogepognmqegoomeqrgmoqergqermgqergopergqewgeqgqerugoeniogengpewogepognmqegoomeqrgmoqergqermgqergopergqewgeqgqerugoeniogengpewogepognmqegoomeqrgmoqergqermgqergopergqewgeqgqerugoeniogengpewogepognmqegoomeqrgmoqergqermgqergopergqewgeqgqerugoeniogengpewogepognmqegoomeqrgmoqergqermgqergopergqewgeqgqerugoeniogengpewogepognmqegoomeqrgmoqergqermgqergopemgqegerqgggggggggggggggggggggg</div>"
+    change(text)
+}
+
